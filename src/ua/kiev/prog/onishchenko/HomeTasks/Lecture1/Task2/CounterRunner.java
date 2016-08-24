e1b3bac124c3ac938afb27323d5b8f2af8297ca1
JIeIIIa <stealth@bigmir.net>
2016-08-25 00:43:18 +0300

    test

--- src/ua/kiev/prog/onishchenko/HomeTasks/Lecture1/Task2/CounterRunner.java
+++ src/ua/kiev/prog/onishchenko/HomeTasks/Lecture1/Task2/CounterRunner.java
@@ -4,6 +4,7 @@ public class CounterRunner {
     public static void main(String[] args) {
         Counter counter1 = new Counter();
         Counter counter2 = new Counter();
+        Counter counter3 = new Counter();
         System.out.println("Objects created: " + new Counter().objectsCreated());
     }
 }
