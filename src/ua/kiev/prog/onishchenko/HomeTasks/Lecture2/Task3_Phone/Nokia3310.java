5c6dba92042cb0ccdf4056d95c4b28efc8d8adf4
JIeIIIa <stealth@bigmir.net>
2016-08-27 02:37:53 +0300

    Homework 2 Task 3 - Phone begin source

---
+++ src/ua/kiev/prog/onishchenko/HomeTasks/Lecture2/Task3_Phone/Nokia3310.java
@@ -0,0 +1,23 @@
+package ua.kiev.prog.onishchenko.HomeTasks.Lecture2.Task3_Phone;
+
+public class Nokia3310 extends Phone {
+	
+	public Nokia3310() {
+		System.out.println("Nokia3310 constructor");
+		
+		touch = false;
+		hasWifi = false;
+		screenSize = 2;
+	}
+	
+	@Override
+	public void call(String number) {
+		super.call(number);
+		System.out.println("Nokia3310 class is calling " + number);
+	}
+	
+	@Override
+	public void sendSMS(String number, String message) {
+		System.out.println("Nokia3310 class is sending sms " + message + " to " + number);	
+	}
+}
